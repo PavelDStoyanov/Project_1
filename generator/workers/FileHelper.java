0956f394cd5d1924f2c7969aa838170efa96e8f3
PavelDStoyanov <161254416+PavelDStoyanov@users.noreply.github.com>
2024-04-01 01:20:29 +0300

    Files

---
+++ generator/workers/FileHelper.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.b1.f22621682.project1.Project_1.generator.workers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileHelper {
+    File file = new File("graphics.txt");
+    Scanner scanner = new Scanner(file);
+
+    public FileHelper() throws FileNotFoundException {
+    }
+}
