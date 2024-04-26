ddb9c77ad41a2b5c279735e1b539272a8461de51
PavelDStoyanov <161254416+PavelDStoyanov@users.noreply.github.com>
2024-04-27 00:17:13 +0300

    Enum menu keys

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package bg.tu_varna.sit.b1.f22621682.project1.Project_1;
 
 import bg.tu_varna.sit.b1.f22621682.project1.Project_1.generator.contracts.commands.FileSystem;
+import bg.tu_varna.sit.b1.f22621682.project1.Project_1.generator.enums.BasicCommands;
 import bg.tu_varna.sit.b1.f22621682.project1.Project_1.generator.workers.commands.implementations.*;
 import bg.tu_varna.sit.b1.f22621682.project1.Project_1.generator.workers.commands.request.FileSystemImpl;
 
@@ -13,40 +14,44 @@ public class Main {
         String choice;
         FileSystemImpl fileSystem = new FileSystemImpl();
         Scanner scanner = new Scanner(System.in);
+        BasicCommands basicCommands;
+
         File file = null;
+
 
         do {
             System.out.println("Choose a command:");
             System.out.println("open, close, save, saveAs, help, exit");
             System.out.print("> ");
             choice = scanner.nextLine();
-            switch (choice) {
-                case "open":
+            basicCommands = BasicCommands.valueOf(choice);
+            switch (basicCommands) {
+                case open:
                     OpenFileCommand openFileCommand = new OpenFileCommand(fileSystem, file);
                     openFileCommand.execute();
                     break;
-                case "close":
+                case close:
                     CloseFileCommand closeFileCommand = new CloseFileCommand(fileSystem);
                     closeFileCommand.execute();
                     break;
-                case "save":
+                case save:
                     SaveFileCommand saveFileCommand = new SaveFileCommand(fileSystem);
                     saveFileCommand.execute();
                     break;
-                case "saveAs":
+                case saveAs:
                     SaveFileAsCommand saveFileAsCommand =  new SaveFileAsCommand(fileSystem);
                     saveFileAsCommand.execute();
                     break;
-                case "help":
+                case help:
                     HelpCommand helpCommand = new HelpCommand(fileSystem);
                     helpCommand.execute();
                     break;
-                case "exit":
+                case exit:
                     ExitCommand exitCommand = new ExitCommand(fileSystem);
                     exitCommand.execute();
                     break;
 
             }
-        } while (!choice.equals("exit"));
+        } while (!basicCommands.equals(BasicCommands.exit));
     }
 }
