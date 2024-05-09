5e33569b863a964d431a29c290a398e9a25da6f1
PavelDStoyanov <161254416+PavelDStoyanov@users.noreply.github.com>
2024-05-09 23:25:11 +0300

    Class Session all methods

--- generator/models/Session.java
+++ generator/models/Session.java
@@ -6,19 +6,31 @@ import java.util.List;
 public class Session {
     private int id;
     private List<Image> images;
+    private List<Transformation> transformations;
 
     public Session(int id) {
-        this.images = new ArrayList();
         this.id = id;
+        this.images = new ArrayList();
+        this.transformations = new ArrayList();
     }
 
     public void addImage(Image image){
         images.add(image);
     }
-    
+
+    public void addTransformation(Transformation transformation){
+        transformations.add(transformation);
+    }
+
     public void transformImages(Transformation transformation){
         for(Image element : images){
             element.applyTransformation(transformation);
+        }
+    }
+
+    public void applyAllTransformations(){
+        for(Transformation element : transformations){
+            this.transformImages(element);
         }
     }
 }
